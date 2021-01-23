472574f0b06c819de829abdb313bda4740bc904d
canan gök <canangok_186@hotmail.com>
2021-01-23 22:25:15 +0300

    plus minus

---
+++ Plus Minus/c#/PlusMinus/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace PlusMinus
+{
+    class Program
+    {
+        static void plusMinus(int[] arr)
+        {
+
+            double negativeCount = 0;
+            double positiveCount = 0;
+            double zeroCount = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    negativeCount++;
+                }
+                else if (arr[i] > 0)
+                {
+                    positiveCount++;
+                }
+                else
+                {
+                    zeroCount++;
+                }
+            }
+            double denominator = Math.Pow(arr.Length, -1);
+            Console.WriteLine((positiveCount * denominator).ToString("0.#####0"));
+            Console.WriteLine((negativeCount * denominator).ToString("0.#####0"));
+            Console.WriteLine((zeroCount * denominator).ToString("0.#####0"));
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            
+            plusMinus(arr);
+        }
+    }
+}
+

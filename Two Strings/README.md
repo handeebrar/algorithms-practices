8940e0e5859be8e72d12d625ff725601e167e455
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-03-15 00:10:46 +0300

    Implement Two Strings solution with C#

--- Two Strings/README.md
+++ Two Strings/README.md
@@ -1,24 +1,27 @@
 ### Two Strings
 Given two strings, determine if they share a common substring. A substring may be as small as one character.
-**Example**
-**s1 = "and"**
-**s2 = "art"**
-These share the common substring ***a***.
+**Example** <br>
+**s1 = "and"** <br>
+**s2 = "art"** <br>
+These share the common substring ***a***. <br>
+<br>
 
-**s1 = "be"**
-**s2 = "cat"**
+**s1 = "be"** <br>
+**s2 = "cat"** <br>
 These do not share a substring.
+
 #### Function description
-Complete the function twoStrings in the editor below. 
-twoStrings has the following parameter(s):
-* string s1: a string
-* string s2: another string
+Complete the function twoStrings in the editor below. <br>
+twoStrings has the following parameter(s): <br>
+* string s1: a string <br>
+* string s2: another string <br>
 
 #### Return
 string: YES or NO
+
 #### Input Format
-The first line contains a single integer ***p***, the number of test cases.
-The following ***p*** pairs of lines are as follows:
+The first line contains a single integer ***p***, the number of test cases. <br>
+The following ***p*** pairs of lines are as follows: <br>
 * The first line contains string ***s1***.
 * The second line contains string ***s2***.
 
--- Two Strings/c#/TwoStrings/Program.cs
+++ Two Strings/c#/TwoStrings/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using System.IO;
+using System;
+using System.Linq;
 
-namespace TwoStrings
+class Solution
 {
-    class Program
+    static string twoStrings(string s1, string s2)
     {
-        static void Main(string[] args)
+        /* 97 == 'a' | 122 == 'z' */
+        for (int leterAscii = 97; leterAscii <= 122; leterAscii++)
+            if (s1.Contains((char)leterAscii) && s2.Contains((char)leterAscii))
+                return "YES";
+        return "NO";
+
+        /* you can solve with this solution
+            var alphabe = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            foreach (var letter in alphabe)
+            {
+                if (s1.Contains(letter) && s2.Contains(letter))
+                    return "YES";
+                return "NO";
+            }
+        */
+    }
+
+    static void Main(string[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++)
         {
-            Console.WriteLine("Hello World!");
+            string s1 = Console.ReadLine();
+
+            string s2 = Console.ReadLine();
+
+            string result = twoStrings(s1, s2);
+
+            Console.WriteLine(result);
         }
     }
 }
